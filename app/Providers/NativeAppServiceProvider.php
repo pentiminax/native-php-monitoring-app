ddc19d7569c13668f30419f4224f784a70b394fb
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-08-24 10:29:10 +0200

    Remove unused use

--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use Native\Laravel\Facades\ContextMenu;
-use Native\Laravel\Facades\Dock;
 use Native\Laravel\Facades\MenuBar;
-use Native\Laravel\Facades\Window;
-use Native\Laravel\GlobalShortcut;
 use Native\Laravel\Menu\Menu;
 
 class NativeAppServiceProvider
